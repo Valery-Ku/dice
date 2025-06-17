e4312265fb341a6ba1165a20136c2badcff4eeb8
Valery-Ku <valeria.valera-valer@yandex.ru>
2025-06-18 01:59:26 +0300

    redacted

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import Dice from './components/dice';
+import Dice from './components/Dice/Dice';
+import diceImage1 from './components/Dice/dice-img/dice 1.png'; 
+import diceImage2 from './components/Dice/dice-img/dice 2.png';
+import diceImage3 from './components/Dice/dice-img/dice 3.png';
+import diceImage4 from './components/Dice/dice-img/dice 4.png';
+import diceImage5 from './components/Dice/dice-img/dice 5.png';
+import diceImage6 from './components/Dice/dice-img/dice 6.png';
+import throwGif from './components/Dice/dice-img/dice-game.gif'; 
+import initialImage from './components/Dice/dice-img/initialImage.png'; 
 
 function App() {
     return (
         <div>
-            <h1 className = "header">Счастливый Кубик</h1>
             <Dice 
                 images={[
-                    "/dice-img/dice 1.png",
-                    "/dice-img/dice 2.png",
-                    "/dice-img/dice 3.png",
-                    "/dice-img/dice 4.png",
-                    "/dice-img/dice 5.png",
-                    "/dice-img/dice 6.png",
+                    diceImage1,
+                    diceImage2,
+                    diceImage3,
+                    diceImage4,
+                    diceImage5,
+                    diceImage6,
                 ]}
-                throwGif="/dice-img/dice-game.gif" 
-                initialImage="/dice-img/initialImage.png"
+                throwGif={throwGif} 
+                initialImage={initialImage}
             />
         </div>
     );
---
+++ src/components/Dice/Dice.js
@@ -0,0 +1,33 @@
+import styles from "./Dice.module.css";
+import React, { useState } from 'react';
+
+export default function Dice({ images, throwGif, initialImage }) {
+    const [currentImage, setCurrentImage] = useState(initialImage);
+    const [isRolling, setIsRolling] = useState(false);
+
+    function rollDice() {
+        setIsRolling(true);
+        setCurrentImage(throwGif); 
+        setTimeout(() => {
+            const index = Math.floor(Math.random() * images.length);
+            setCurrentImage(images[index]);
+            setIsRolling(false);
+        }, 1000);
+    }
+
+    return (
+        <div className={styles.Dice}>
+            <div className={styles.header}>Счастливый кубик</div> {}
+            <div className={styles.Dice__images}>
+                {isRolling ? (
+                    <img className={styles.Dice__gif} src={currentImage} alt="Гиф кубика" />
+                ) : (
+                    <img className={styles.Dice__part} src={currentImage} alt="Результат броска" />
+                )}
+            </div>
+            <div className={styles.Button}>
+                <button onClick={rollDice} className={styles.ThrowDice} aria-label="Бросить кубик">Бросок!</button>
+            </div>
+        </div>
+    );
+}
